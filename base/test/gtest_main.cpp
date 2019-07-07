bdc30c31607b6cfea43c8efeb92e2eb836c893f6
alexsmn <alexsmn@gmail.com>
2019-07-07 11:02:12 -0600

    Fix scada_model UT.

---
+++ base/test/gtest_main.cpp
@@ -0,0 +1,18 @@
+#include "base/boost_log.h"
+#include "base/files/file_path.h"
+#include "base/path_service.h"
+
+#include <gmock/gmock.h>
+
+int main(int argc, char** argv) {
+  {
+    base::FilePath path;
+    if (!base::PathService::Get(base::DIR_EXE, &path))
+      return 1;
+    path = path.AppendASCII("logs");
+    InitBoostLogging(path.value(), true);
+  }
+
+  ::testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
+}
