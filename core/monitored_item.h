6330c75603d31a0687cbd8842922c9cae4eb935a
alexsmn <alexsmn@gmail.com>
2019-07-08 20:03:41 -0600

    Update to dev.

--- core/monitored_item.h
+++ core/monitored_item.h
@@ -1,14 +1,13 @@
 #pragma once
 
+#include "core/configuration_types.h"
+
 #include <any>
 #include <functional>
-
-#include "core/configuration_types.h"
 
 namespace scada {
 
 class DataValue;
-class Event;
 class Status;
 
 typedef std::function<void(const DataValue& data_value)> DataChangeHandler;
@@ -20,24 +19,8 @@ class MonitoredItem {
   MonitoredItem() {}
   virtual ~MonitoredItem() {}
 
-  MonitoredItem(const MonitoredItem&) = delete;
-  MonitoredItem& operator=(const MonitoredItem&) = delete;
-
-  void set_data_change_handler(DataChangeHandler handler) {
-    data_change_handler_ = std::move(handler);
-  }
-  void set_event_handler(EventHandler handler) {
-    event_handler_ = std::move(handler);
-  }
-
-  virtual void Subscribe() = 0;
-
-  void ForwardData(const DataValue& data_value);
-  void ForwardEvent(const Status& status, const std::any& event);
-
- protected:
-  DataChangeHandler data_change_handler_;
-  EventHandler event_handler_;
+  virtual void SubscribeData(DataChangeHandler handler) = 0;
+  virtual void SubscribeEvents(EventHandler handler) = 0;
 };
 
 }  // namespace scada
