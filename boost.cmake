155f725d396d5b7c42acfcdff1af5a2af4757f53
alexsmn <alexsmn@gmail.com>
2019-09-14 17:41:10 -0600

    Fix Boost deployment.

--- boost.cmake
+++ boost.cmake
@@ -1,14 +1,22 @@
-set(BOOST_SUFFIX "vc142-mt$<$<CONFIG:Debug>:-gd>-x32-${Boost_LIB_VERSION}")
-# TODO: Remove Vcpkg dependency.
-set(BOOST_DLL_DIR "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}$<$<CONFIG:Debug>:/debug>/bin")
+set(BOOST_COMPONENTS system thread log log_setup filesystem date_time regex program_options)
 
-macro(list_boost_dlls DLLS)
-  foreach(BOOST_COMPONENT ${ARGN})
-    list(APPEND ${DLLS} "${BOOST_DLL_DIR}/boost_${BOOST_COMPONENT}-${BOOST_SUFFIX}.dll")
-  endforeach()
-endmacro()
+find_package(Boost REQUIRED COMPONENTS ${BOOST_COMPONENTS})
 
-list_boost_dlls(BOOST_DLLS ${BOOST_COMPONENTS})
-install(FILES ${BOOST_DLLS}
-  DESTINATION bin
-  COMPONENT Common)
+if(WIN32)
+  set(BOOST_SUFFIX "vc142-mt$<$<CONFIG:Debug>:-gd>-x32-${Boost_LIB_VERSION}")
+  # TODO: Remove Vcpkg dependency.
+  set(BOOST_DLL_DIR "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}$<$<CONFIG:Debug>:/debug>/bin")
+
+  macro(list_boost_dlls DLLS)
+    foreach(BOOST_COMPONENT ${ARGN})
+      list(APPEND ${DLLS} "${BOOST_DLL_DIR}/boost_${BOOST_COMPONENT}-${BOOST_SUFFIX}.dll")
+    endforeach()
+  endmacro()
+
+  message("Install Boost components: ${BOOST_COMPONENTS}")
+  list_boost_dlls(BOOST_DLLS ${BOOST_COMPONENTS})
+  install(FILES ${BOOST_DLLS}
+    DESTINATION bin
+    COMPONENT Common)
+
+endif(WIN32)
