8f0b728ee5b9ff80e446ab354e6a99eab65c11be
alexsmn <alexsmn@gmail.com>
2019-10-05 14:46:54 -0600

    Add NetBoostLoggerAdapter.

---
+++ base/net_boost_logger_adapter.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "base/boost_log.h"
+#include "base/strings/stringprintf.h"
+#include "net/logger.h"
+
+class NetBoostLoggerAdapter final : public net::Logger {
+ public:
+  explicit NetBoostLoggerAdapter(BoostLogger& boost_logger)
+      : boost_logger_{boost_logger} {}
+
+  virtual void Write(net::LogSeverity severity,
+                     const char* message) const override {
+    BOOST_LOG_SEV(boost_logger_, ToBoostLogSeverity(severity)) << message;
+  }
+
+  virtual void WriteV(net::LogSeverity severity,
+                      const char* format,
+                      va_list args) const override {
+    BOOST_LOG_SEV(boost_logger_, ToBoostLogSeverity(severity))
+        << base::StringPrintV(format, args);
+  }
+
+  virtual void WriteF(net::LogSeverity severity,
+                      const char* format,
+                      ...) const override {
+    va_list args;
+    va_start(args, format);
+    BOOST_LOG_SEV(boost_logger_, ToBoostLogSeverity(severity))
+        << base::StringPrintV(format, args);
+    va_end(args);
+  }
+
+ private:
+  static BoostLogSeverity ToBoostLogSeverity(net::LogSeverity severity) {
+    switch (severity) {
+      case net::LogSeverity::Normal:
+        return BoostLogSeverity::Info;
+      case net::LogSeverity::Warning:
+        return BoostLogSeverity::Warning;
+      case net::LogSeverity::Error:
+        return BoostLogSeverity::Error;
+      case net::LogSeverity::Fatal:
+        return BoostLogSeverity::Critical;
+      default:
+        assert(false);
+        return BoostLogSeverity::Info;
+    }
+  }
+
+  BoostLogger& boost_logger_;
+};
