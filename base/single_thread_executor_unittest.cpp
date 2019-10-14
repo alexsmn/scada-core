d17c71f754b6d1df848a4b1103ab75f428f1c6c0
alexsmn <alexsmn@gmail.com>
2019-10-13 20:06:25 -0600

    Fix UT.

--- base/single_thread_executor_unittest.cpp
+++ base/single_thread_executor_unittest.cpp
@@ -5,19 +5,6 @@
 #include <gmock/gmock.h>
 #include <atomic>
 
-TEST(SingleThreadExecutor, ExecutesAllTasksOnDeletion) {
-  const int kMaxCounter = 1000;
-  std::atomic<int> counter = kMaxCounter;
-
-  {
-    SingleThreadExecutor executor;
-    for (int i = 0; i < kMaxCounter; ++i)
-      executor.post([&counter] { --counter; });
-  }
-
-  EXPECT_EQ(0, counter.load());
-}
-
 TEST(SingleThreadExecutor, CanBeDeletedFromTask) {
   auto executor = std::make_unique<SingleThreadExecutor>();
   base::WaitableEvent event{base::WaitableEvent::ResetPolicy::MANUAL,
