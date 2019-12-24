56424dd69dd20105e00adaa71c6853149eec5a65
alexsmn <alexsmn@gmail.com>
2019-12-24 11:26:09 -0700

    Update GTest.

---
+++ gtest.cmake
@@ -0,0 +1,16 @@
+include(GoogleTest)
+
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+set(GTEST_FOUND TRUE CACHE BOOL "" FORCE)
+
+add_subdirectory("${deps}/googletest" gtest EXCLUDE_FROM_ALL)
+
+target_compile_definitions(gtest PUBLIC
+  -DGTEST_HAS_TR1_TUPLE=0
+  -DGTEST_USE_OWN_TR1_TUPLE=0
+  -DGTEST_LANG_CXX11=1
+)
+
+set(GTEST_INCLUDE_DIRS "${deps}/googletest/googletest/include")
+set(GMOCK_INCLUDE_DIRS "${deps}/googletest/googlemock/include")
