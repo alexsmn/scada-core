de50807993283b503ea18cf9f509f6aac14f4e9b
alexsmn <alexsmn@gmail.com>
2019-12-24 10:05:03 -0700

    Improve FormatHResult.

--- base/win/format_hresult.cpp
+++ base/win/format_hresult.cpp
@@ -1,23 +1,22 @@
+Ôªø#include "base/win/format_hresult.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/win/format_hresult.h"
 
 #include <cassert>
 #include <windows.h>
 
 base::string16 FormatHresultUTF16(HRESULT err) {
-  wchar_t* buf = NULL;
-  if (!FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER |
-                      FORMAT_MESSAGE_FROM_SYSTEM,
-                      NULL, err, 0, reinterpret_cast<LPWSTR>(&buf), 0, NULL))
-    return base::StringPrintf(L"Œ¯Ë·Í‡ %d", err);
+  LPWSTR buf = NULL;
+  if (!FormatMessageW(
+          FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL,
+          err, 0, reinterpret_cast<LPWSTR>(&buf), 0, NULL))
+    return base::StringPrintf(L"–û—à–∏–±–∫–∞ Windows %d", err);
 
   assert(buf);
 
   // Delete ending line breaks.
   size_t len = wcslen(buf);
-  while (len && (buf[len - 1] == L'\r' ||
-                 buf[len - 1] == L'\n' ||
+  while (len && (buf[len - 1] == L'\r' || buf[len - 1] == L'\n' ||
                  buf[len - 1] == L'.')) {
     len--;
   }
@@ -28,5 +27,22 @@ base::string16 FormatHresultUTF16(HRESULT err) {
 }
 
 std::string FormatHresult(HRESULT err) {
-  return base::SysWideToNativeMB(FormatHresultUTF16(err));
+  LPSTR buf = NULL;
+  if (!FormatMessageA(
+          FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL,
+          err, 0, reinterpret_cast<LPSTR>(&buf), 0, NULL))
+    return base::StringPrintf("Windows error %d", err);
+
+  assert(buf);
+
+  // Delete ending line breaks.
+  size_t len = strlen(buf);
+  while (len && (buf[len - 1] == '\r' || buf[len - 1] == '\n' ||
+                 buf[len - 1] == '.')) {
+    len--;
+  }
+
+  std::string str(buf, len);
+  LocalFree(buf);
+  return str;
 }
