9c5436d39a0bbc0d8e1f37e4e488be8736771bd8
alexsmn <alexsmn@gmail.com>
2020-12-10 21:57:24 -0700

    Fix AsioExecutor.

--- base/asio_executor.h
+++ base/asio_executor.h
@@ -1,22 +1,21 @@
 #pragma once
 
 #include "base/executor.h"
+#include "base/io_context_util.h"
 
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/io_context_strand.hpp>
 
-class AsioStrandExecutor : public Executor {
+class AsioExecutor : public Executor {
  public:
-  explicit AsioStrandExecutor(boost::asio::io_context& io_context)
+  explicit AsioExecutor(boost::asio::io_context& io_context)
       : io_context_{io_context} {}
 
   virtual void PostDelayedTask(Duration delay, Task task) override {
-    strand_.post(task);
+    ::PostDelayedTask(io_context_, delay, std::move(task));
   }
 
   virtual size_t GetTaskCount() const override { return 0; }
 
  private:
   boost::asio::io_context& io_context_;
-  boost::asio::io_context::strand strand_{io_context_};
 };
