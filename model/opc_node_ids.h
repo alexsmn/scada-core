a270b5b6a895f63c62262691fee68edfc81618ad
alexsmn <alexsmn@gmail.com>
2021-02-07 15:06:27 -0700

    Introduce OPC module.

---
+++ model/opc_node_ids.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "core/node_id.h"
+#include "model/namespaces.h"
+
+namespace opc {
+
+namespace id {
+
+// TODO: Move to OPC module.
+const scada::NodeId OPC{329, NamespaceIndexes::SCADA};
+
+} // namespace id
+
+}
