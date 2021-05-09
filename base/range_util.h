3870f75e3bfce0c09ab794b9b6ddc97e0e4017a6
alexsmn <alexsmn@gmail.com>
2021-05-08 19:51:29 -0600

    Add Erase() range util.

--- base/range_util.h
+++ base/range_util.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <map>
 #include <set>
 #include <vector>
 
@@ -33,4 +34,23 @@ inline auto Map(const Range& range, const Func& func) {
   std::transform(std::cbegin(range), std::cend(range),
                  std::back_inserter(result), func);
   return result;
+}
+
+template <class E, class T>
+inline bool Erase(std::vector<E>& vector, const T& item) {
+  auto i = std::find(std::begin(vector), std::end(vector), item);
+  if (i == std::end(vector))
+    return false;
+  vector.erase(i);
+  return true;
+}
+
+template <class K, class V, class T>
+inline bool Erase(std::map<K, V>& map, const T& item) {
+  return map.erase(item) != 0;
+}
+
+template <class K, class T>
+inline bool Erase(std::set<K>& set, const T& item) {
+  return set.erase(item) != 0;
 }
