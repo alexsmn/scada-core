d8b368f25ce4df632451e2bede06c4860ee7ccc1
alexsmn <alexsmn@gmail.com>
2021-06-06 12:01:51 -0600

    Revert boost script.

--- boost.cmake
+++ boost.cmake
@@ -3,17 +3,17 @@ set(BOOST_COMPONENTS thread log log_setup filesystem date_time regex program_opt
 find_package(Boost REQUIRED COMPONENTS ${BOOST_COMPONENTS})
 
 if(WIN32)
-  set(BOOST_SUFFIX "vc142-mt$<$<CONFIG:Debug>:-gd>-x32-${Boost_LIB_VERSION}")
+  set(BOOST_DEBUG_SUFFIX "vc142-mt-gd-x32-${Boost_LIB_VERSION}")
+  set(BOOST_RELEASE_SUFFIX "vc142-mt-x32-${Boost_LIB_VERSION}")
 
-  set(BOOST_DLL_DIR "${LIBRARY_OUTPUT_PATH}/${CMAKE_BUILD_TYPE}")
   foreach(BOOST_COMPONENT ${BOOST_COMPONENTS})
     if(NOT ${BOOST_COMPONENT} MATCHES "date_time")
-      list(APPEND BOOST_DLLS "${BOOST_DLL_DIR}/boost_${BOOST_COMPONENT}-${BOOST_SUFFIX}.dll")
+      install(FILES
+          "$<$<CONFIG:Debug>:${LIBRARY_OUTPUT_PATH}/Debug/boost_${BOOST_COMPONENT}-${BOOST_DEBUG_SUFFIX}.dll>"
+          "$<$<CONFIG:RelWithDebInfo>:${LIBRARY_OUTPUT_PATH}/RelWithDebInfo/boost_${BOOST_COMPONENT}-${BOOST_RELEASE_SUFFIX}.dll>"
+        DESTINATION bin
+        COMPONENT Common)
     endif()
   endforeach()
-
-  install(FILES ${BOOST_DLLS}
-    DESTINATION bin
-    COMPONENT Common)
 
 endif(WIN32)
