d84f22baa43642efdb6b4a4a952c8716714b4f0d
alexsmn <alexsmn@gmail.com>
2022-06-11 12:59:52 -0700

    Add scada::MakeStatusCodeOr.

--- core/status_or.h
+++ core/status_or.h
@@ -10,6 +10,18 @@ using StatusOr = std::pair<Status, T>;
 template <class T>
 using StatusCodeOr = std::pair<StatusCode, T>;
 
+template <class T>
+inline auto MakeStatusCodeOr(T&& value) {
+  return StatusCodeOr<std::decay_t<T>>{scada::StatusCode::Good,
+                                       std::move(value)};
+}
+
+template <class T>
+inline auto MakeStatusCodeOr(scada::StatusCode error_code) {
+  assert(scada::IsBad(error_code));
+  return StatusCodeOr<T>{error_code, {}};
+}
+
 template <class T, class F>
 inline auto MapStatusCodeOr(const scada::StatusCodeOr<T>& value, F&& f) {
   using R = std::invoke_result_t<F, T>;
