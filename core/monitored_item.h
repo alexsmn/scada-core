cae4c6732e7a2ae5871a5700bf5a17215382769e
alexsmn <alexsmn@gmail.com>
2023-04-22 13:09:04 -0700

    Document MonitoredItem handlers.

--- core/monitored_item.h
+++ core/monitored_item.h
@@ -9,9 +9,15 @@ namespace scada {
 class DataValue;
 class Status;
 
+// When first subscribed, an initial value is sent. Initial value may be null.
 using DataChangeHandler = std::function<void(const DataValue& data_value)>;
+
+// When first subscribed, an initial `status` update is sent. Initial status
+// update may have or may not have an `event`. WARNING: `event` may be not set
+// for initial status update.
 using EventHandler =
     std::function<void(const Status& status, const std::any& event)>;
+
 using MonitoredItemHandler = std::variant<DataChangeHandler, EventHandler>;
 
 class MonitoredItem {
