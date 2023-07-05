34437e2c2aeb8f8df4b1310df59a11ac53421571
alexsmn <alexsmn@gmail.com>
2023-07-04 18:17:42 -0700

    CMake: Extract `scada_module_unittests` macro.

--- scada_module.cmake
+++ scada_module.cmake
@@ -1,3 +1,25 @@
+macro(scada_module_unittests MODULE_NAME)
+  cmake_parse_arguments(ARG "" "" "SOURCES" ${ARGN})
+
+  if (ARG_SOURCES)
+    message("scada_module_unittests(${MODULE_NAME})")
+
+    if (NOT TARGET ${MODULE_NAME}_unittests)
+      add_executable(${MODULE_NAME}_unittests)
+      target_link_libraries(${MODULE_NAME}_unittests PUBLIC ${MODULE_NAME} base_unittest)
+      set_property(TARGET ${MODULE_NAME}_unittests PROPERTY FOLDER ${scada_folder})
+      include(GoogleTest)
+      # Temporary workaround of "no cmake script provided" error. Also,
+      # `gtest_discover_tests` is not correctly handled by CTest. So you cannot test Linux builds.
+      # In opposite, `gtest_add_tests` triggers generation on each UT modification.
+      gtest_discover_tests(${MODULE_NAME}_unittests DISCOVERY_MODE PRE_TEST)
+      # gtest_add_tests(TARGET ${MODULE_NAME}_unittests)
+    endif()
+
+    target_sources(${MODULE_NAME}_unittests PUBLIC ${ARG_SOURCES})
+  endif()
+endmacro()
+
 macro(scada_module_sources MODULE_NAME SOURCE_DIR)
   set(OPTIONS "RECURSE")
   set(ONE_VALUE_ARGS "")
@@ -10,7 +32,7 @@ macro(scada_module_sources MODULE_NAME SOURCE_DIR)
     set(SCADA_MODULE_GLOB "GLOB")
   endif()
 
-  message("${MODULE_NAME}: ${SCADA_MODULE_GLOB} sources under ${SOURCE_DIR}")
+  message("scada_module_sources(${MODULE_NAME} ${SOURCE_DIR} ${SCADA_MODULE_GLOB})")
 
   file(${SCADA_MODULE_GLOB} ${MODULE_NAME}_SOURCES CONFIGURE_DEPENDS "${SOURCE_DIR}/*.cpp" "${SOURCE_DIR}/*.h")
   file(${SCADA_MODULE_GLOB} ${MODULE_NAME}_UT_SOURCES CONFIGURE_DEPENDS "${SOURCE_DIR}/*_unittest.*" "${SOURCE_DIR}/*_mock.*")
@@ -28,24 +50,12 @@ macro(scada_module_sources MODULE_NAME SOURCE_DIR)
 
   target_sources(${MODULE_NAME} PRIVATE ${${MODULE_NAME}_SOURCES})
 
-  if (${MODULE_NAME}_UT_SOURCES)
-    if (NOT TARGET ${MODULE_NAME}_UT_SOURCES)
-      add_executable(${MODULE_NAME}_unittests ${${MODULE_NAME}_UT_SOURCES})
-      target_link_libraries(${MODULE_NAME}_unittests PUBLIC ${MODULE_NAME} base_unittest)
-      set_property(TARGET ${MODULE_NAME}_unittests PROPERTY FOLDER ${scada_folder})
-      include(GoogleTest)
-      # Temporary workaround of "no cmake script provided" error. Also,
-      # `gtest_discover_tests` is not correctly handled by CTest. So you cannot test Linux builds.
-      # In opposite, `gtest_add_tests` triggers generation on each UT modification.
-      gtest_discover_tests(${MODULE_NAME}_unittests DISCOVERY_MODE PRE_TEST)
-      # gtest_add_tests(TARGET ${MODULE_NAME}_unittests)
-    endif()
-
-    target_sources(${MODULE_NAME}_unittests PUBLIC ${${MODULE_NAME}_UT_SOURCES})
-  endif()
+  scada_module_unittests(${MODULE_NAME} SOURCES ${${MODULE_NAME}_UT_SOURCES})
 endmacro()
 
 macro(scada_module MODULE_NAME)
+  message("scada_module(${MODULE_NAME})")
+
   add_library(${MODULE_NAME})
   set_property(TARGET ${MODULE_NAME} PROPERTY FOLDER ${scada_folder})
 
