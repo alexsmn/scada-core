750c0e07e433f6416e7a896eea931169e9a05a2e
alexsmn <alexsmn@gmail.com>
2023-07-26 17:54:29 -0700

    Rename network_test_environment.h.

--- remote/remote_services_unittest.cpp
+++ remote/remote_services_unittest.cpp
@@ -2,13 +2,13 @@
 
 #include "base/logger.h"
 #include "base/test/asio_test_environment.h"
-#include "base/test/network_port_generator.h"
+#include "base/test/network_test_environment.h"
 #include "base/test/test_executor.h"
+#include "remote/remote_session_manager.h"
 #include "scada/authentication_mock.h"
 #include "scada/client.h"
 #include "scada/data_services_factory.h"
 #include "scada/monitored_item_service_mock.h"
-#include "remote/remote_session_manager.h"
 
 #include <gmock/gmock.h>
 
