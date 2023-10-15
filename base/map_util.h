2016e4d877dac9c8043143e348c6bcdb633245ee
alexsmn <alexsmn@gmail.com>
2023-10-15 12:04:00 -0700

    Add map utilities.

---
+++ base/map_util.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <ranges>
+#include <vector>
+
+namespace internal {
+
+template <typename Map>
+using KeyType = typename Map::key_type;
+
+template <typename Map>
+using MappedType = typename Map::mapped_type;
+
+}  // namespace internal
+
+template <typename Map, typename Key>
+constexpr const internal::MappedType<Map>* FindOrNull(const Map& map,
+                                                      const Key& key) {
+  auto it = map.find(key);
+  return it != map.end() ? &it->second : nullptr;
+}
+
+template <typename Map, typename Key>
+constexpr internal::MappedType<Map>* FindOrNull(Map& map, const Key& key) {
+  auto it = map.find(key);
+  return it != map.end() ? &it->second : nullptr;
+}
+
+template <class Map>
+inline std::vector<internal::KeyType<Map>> GetKeyVector(const Map& map) {
+  std::vector<internal::KeyType<Map>> result;
+  result.reserve(map.size());
+  std::ranges::copy(map | std::views::keys, std::back_inserter(result));
+  return result;
+}
