7cd14c6ab4056c345c61f6943fd400c2361ca6fe
alexsmn <alexsmn@gmail.com>
2023-11-04 12:57:00 -0700

    Add `BindPromiseExecutorWithResult` UT.

---
+++ base/promise_executor_unittest.cpp
@@ -0,0 +1,31 @@
+#include "base/promise_executor.h"
+
+#include "base/test/test_executor.h"
+
+#include <gmock/gmock.h>
+
+TEST(BindPromiseExecutorWithResult, VoidReturnType) {
+  auto executor = std::make_shared<TestExecutor>();
+  std::function<promise<void>()> binding = BindPromiseExecutorWithResult(
+      executor, [] { return make_resolved_promise(); });
+
+  binding().get();
+}
+
+TEST(BindPromiseExecutorWithResult, IntReturnType) {
+  auto executor = std::make_shared<TestExecutor>();
+  std::function<promise<int>()> binding = BindPromiseExecutorWithResult(
+      executor, [] { return make_resolved_promise(42); });
+
+  EXPECT_EQ(binding().get(), 42);
+}
+
+TEST(BindPromiseExecutorWithResult, VoidReturnTypeWithCancelation) {
+  auto executor = std::make_shared<TestExecutor>();
+  auto cancelation = std::make_shared<int>();
+  std::function<promise<void>()> binding =
+      BindPromiseExecutorWithResult(executor, std::weak_ptr{cancelation},
+                                    [] { return make_resolved_promise(); });
+
+  binding().get();
+}
