ad8942787239044e0edaead7e18a3c9623ae8db3
alexsmn <alexsmn@gmail.com>
2023-11-15 19:45:14 -0800

    Add `TruncateTimeTo`.

--- base/time_utils.h
+++ base/time_utils.h
@@ -26,3 +26,12 @@ inline auto InSeconds(std::chrono::nanoseconds duration) {
 inline auto AsChrono(base::TimeDelta delta) {
   return std::chrono::nanoseconds{delta.InNanoseconds()};
 }
+
+// Example of truncation to a second:
+//   TruncateTimeTo(base::Time::Now(), base::TimeDelta::FromSeconds(1));
+inline base::Time TruncateTimeTo(base::Time time, base::TimeDelta interval) {
+  auto delta = time - base::Time::UnixEpoch();
+  auto remainder = base::TimeDelta::FromNanoseconds(delta.InNanoseconds() %
+                                                    interval.InNanoseconds());
+  return time - remainder;
+}
