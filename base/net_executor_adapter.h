60ab3dd2b029c4bbdebf5d29c66a226065f775af
alexsmn <alexsmn@gmail.com>
2024-01-27 12:04:52 -0800

    Add NetExecutorAdapter.

---
+++ base/net_executor_adapter.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "base/executor.h"
+
+#include <memory>
+#include <net/executor.h>
+
+class NetExecutorAdapter {
+ public:
+  explicit NetExecutorAdapter(std::shared_ptr<Executor> executor)
+      : context_{std::make_shared<boost::asio::execution_context>()},
+        executor_{std::move(executor)} {}
+
+  bool operator==(const NetExecutorAdapter&) const noexcept = default;
+
+  boost::asio::execution_context& query(
+      boost::asio::execution::context_t) const noexcept {
+    return *context_;
+  }
+
+  static constexpr boost::asio::execution::blocking_t::never_t query(
+      boost::asio::execution::blocking_t) noexcept {
+    // This executor always has blocking.never semantics.
+    return boost::asio::execution::blocking.never;
+  }
+
+  template <class F>
+  void execute(F&& f) const {
+    executor_->PostTask(MakeExecutorTask(std::forward<F>(f)));
+  }
+
+ private:
+  template <class F>
+  static Executor::Task MakeExecutorTask(F f) {
+    return [copyable_fun = std::make_shared<F>(std::move(f))]() mutable {
+      std::move (*copyable_fun)();
+    };
+  }
+
+  std::shared_ptr<boost::asio::execution_context> context_;
+  std::shared_ptr<Executor> executor_;
+};
+
+static_assert(boost::asio::execution::is_executor_v<NetExecutorAdapter>);
