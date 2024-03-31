dd7203a8b2c2b4b2a4771250a081e71559ada628
alexsmn <alexsmn@gmail.com>
2024-03-31 11:41:02 -0700

    Minor updates.

--- scada/data_services.h
+++ scada/data_services.h
@@ -4,26 +4,9 @@
 
 #include <memory>
 
-namespace scada {
-class NodeManagementService;
-class HistoryService;
-class AttributeService;
-class MethodService;
-class MonitoredItemService;
-class SessionService;
-class ViewService;
-}  // namespace scada
-
+// TODO: Move under `scada` namespace.
 // TODO: Replace with `std::shared_ptr<scada::services>`.
 struct DataServices {
-  std::shared_ptr<scada::SessionService> session_service_;
-  std::shared_ptr<scada::ViewService> view_service_;
-  std::shared_ptr<scada::NodeManagementService> node_management_service_;
-  std::shared_ptr<scada::HistoryService> history_service_;
-  std::shared_ptr<scada::AttributeService> attribute_service_;
-  std::shared_ptr<scada::MethodService> method_service_;
-  std::shared_ptr<scada::MonitoredItemService> monitored_item_service_;
-
   scada::services as_services() const {
     return {.attribute_service = attribute_service_.get(),
             .monitored_item_service = monitored_item_service_.get(),
@@ -33,4 +16,54 @@ struct DataServices {
             .node_management_service = node_management_service_.get(),
             .session_service = session_service_.get()};
   }
+
+  std::shared_ptr<const scada::services> CreateSharedServices() const {
+    struct Holder {
+      DataServices data_services;
+      scada::services services{data_services.as_services()};
+    };
+
+    auto holder = std::make_shared<Holder>(*this);
+    return std::shared_ptr<const scada::services>(holder, &holder->services);
+  }
+
+  // The `services` are owned by the caller and must outlast the returned
+  // instance.
+  static DataServices FromUnownedServices(const scada::services& services) {
+    // Create a fake pointer.
+    return FromSharedServices(std::make_shared<scada::services>(services));
+  }
+
+  static DataServices FromSharedServices(
+      const std::shared_ptr<const scada::services>& shared_services) {
+    return {
+        .session_service_ =
+            std::shared_ptr<scada::SessionService>{
+                shared_services, shared_services->session_service},
+        .view_service_ =
+            std::shared_ptr<scada::ViewService>{shared_services,
+                                                shared_services->view_service},
+        .node_management_service_ =
+            std::shared_ptr<scada::NodeManagementService>{
+                shared_services, shared_services->node_management_service},
+        .history_service_ =
+            std::shared_ptr<scada::HistoryService>{
+                shared_services, shared_services->history_service},
+        .attribute_service_ =
+            std::shared_ptr<scada::AttributeService>{
+                shared_services, shared_services->attribute_service},
+        .method_service_ =
+            std::shared_ptr<scada::MethodService>{
+                shared_services, shared_services->method_service},
+        .monitored_item_service_ = std::shared_ptr<scada::MonitoredItemService>{
+            shared_services, shared_services->monitored_item_service}};
+  }
+
+  std::shared_ptr<scada::SessionService> session_service_;
+  std::shared_ptr<scada::ViewService> view_service_;
+  std::shared_ptr<scada::NodeManagementService> node_management_service_;
+  std::shared_ptr<scada::HistoryService> history_service_;
+  std::shared_ptr<scada::AttributeService> attribute_service_;
+  std::shared_ptr<scada::MethodService> method_service_;
+  std::shared_ptr<scada::MonitoredItemService> monitored_item_service_;
 };
---
+++ scada/services_mock.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "scada/monitored_item_service_mock.h"
+#include "scada/session_service_mock.h"
+
+namespace scada {
+
+struct MockServices {
+  MockServices() {
+    using namespace testing;
+
+    ON_CALL(session_service, Disconnect())
+        .WillByDefault(Return(make_resolved_promise()));
+  }
+
+  services services() {
+    return {.monitored_item_service = &monitored_item_service,
+            .session_service = &session_service};
+  }
+
+  testing::NiceMock<MockMonitoredItemService> monitored_item_service;
+  testing::NiceMock<MockSessionService> session_service;
+};
+
+}  // namespace scada
