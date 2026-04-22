f9939d7b07ac5b179a2a4618e691a4262a31e1db
alexsmn <alexsmn@gmail.com>
2026-04-22 14:47:03 -0700

    Add packaging metadata contract

--- package.cmake
+++ package.cmake
@@ -1,5 +1,7 @@
 scada_register_package(
   NAME scada-core
   REPO core
+  VERSION "${SCADA_PACKAGING_VERSION}"
   METADATA_ONLY
-  DESCRIPTION "Core runtime package metadata")
+  DESCRIPTION "Core runtime package metadata"
+  RUNTIME_DEPENDENCY_POLICY "provided-transitively")
