fbc0e47a547a50b8502ff1409654c88c501c9887
alexsmn <alexsmn@gmail.com>
2026-04-22 17:45:19 -0700

    Add package distribution metadata

--- package.cmake
+++ package.cmake
@@ -4,4 +4,6 @@ scada_register_package(
   VERSION "${SCADA_PACKAGING_VERSION}"
   METADATA_ONLY
   DESCRIPTION "Core runtime package metadata"
+  DISTRIBUTION_KIND "metadata-only"
+  STANDALONE_INSTALLER_DEFAULT OFF
   RUNTIME_DEPENDENCY_POLICY "provided-transitively")
